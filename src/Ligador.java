b12e328c8422dff578c1517bb49caf08a6ab48ce
Vitor Borges <vitor27.sab@hotmail.com>
2021-05-08 21:47:37 -0300

    nome de algumas classes alteradas para os nomes corretos.

---
+++ src/Ligador.java
@@ -0,0 +1,24 @@
+package src;
+
+import src.memory.Regist;
+import src.montador.asm;
+
+public class main {
+
+    public static Regist registradores = new Regist();
+
+    public static void main(String[] args) {
+
+        //asm = parser + montador
+        asm file = new asm();
+
+        file.asmReader("F:\\Documentos\\CC\\8° semestre - EAD\\PS\\arquivos\\Projeto\\ex1.asm");
+
+        //executa com a maquina
+    }
+/*
+    public Regist getRegister(){
+        return registradores;
+    }
+    */
+}
