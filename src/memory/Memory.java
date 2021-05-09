fbf18049715ff15f2c772468ccf8d0728ad7ac54
Vitor Borges <vitor27.sab@hotmail.com>
2021-05-09 18:05:04 -0300

    memoria preenchida

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -2,11 +2,21 @@ package src.memory;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class Memory {
     List<HashMap<String, Byte>> memory = new ArrayList<HashMap<String, Byte>>();
+    final int SIZEMEMORY = 1000;
     //byte[] memory = new byte[1000];
+
+    public Memory(){
+        HashMap<String, Byte> value = new HashMap<String, Byte>();
+
+        for(int position = 0; position != SIZEMEMORY; position ++){
+            value.put(String.valueOf(position), null);
+            memory.add(value);
+        }
+    }
 
     int getByte(int addr){
         return 0;
