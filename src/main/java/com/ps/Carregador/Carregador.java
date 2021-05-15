b8be25081f95a81bfdc2456b1ba9bf60f406869c
arturmelchiori <arturmelchiori@gmail.com>
2021-05-15 13:22:45 -0300

    initial commit

---
+++ src/main/java/com/ps/Carregador/Carregador.java
@@ -0,0 +1,20 @@
+package com.ps.Carregador;
+
+import com.ps.Memory.Memory;
+
+import java.util.List;
+
+public class Carregador {
+    private Memory memoria;
+
+    public Carregador(Memory memoria){
+        this.memoria = memoria;
+    }
+
+    public void store(List<String> listCodObj){
+        for(String codObj: listCodObj){
+            this.memoria.save(codObj);
+        }
+    }
+
+}
